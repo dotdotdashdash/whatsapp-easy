4eb00c3aeabca44ea8783822ecf3c784af46641c
Ismail <ismailir@alokin.in>
2023-06-18 15:01:22 +0530

    Configure vite to work the site as PWA

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,52 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+
 export default defineConfig({
   base: `/whatsapp-easy/`,
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      devOptions: { enabled: true },
+      workbox: { globPatterns: ['**/*.{js,css,html,ico,png,svg}'] },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'WhatsApp Easy | WhatsApp without saving contact',
+        short_name: 'WhatsApp-Easy',
+        description: 'Send messages to any one on WhatsApp without adding contact',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/images/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/images/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/images/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: '/images/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ],
+        "display": "standalone"
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
