f415871e4b0567adb01a335674e2c673ac699241
Ismail <ismailir@alokin.in>
2023-07-18 13:02:20 +0530

    modify base url in vite-config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: `/whatsapp-easy`,
+  base: ``,
   plugins: [
     vue(),
     VitePWA({
